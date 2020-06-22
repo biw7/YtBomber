ed0b8a15e6ad809c3e843eeb8abf075649659417
W.Carlos 'biwxx <willgiovanetti@hotmail.com>
2020-06-22 14:54:39 -0300

    Add files via upload
    You can get unlimited views with these until the yt algorithm changes to block views from envidious.

---
+++ viewer.py
@@ -0,0 +1,17 @@
+import time
+from tbselenium.tbdriver import TorBrowserDriver
+from os.path import dirname, join, realpath, getsize
+
+
+for i in range (50):
+	with TorBrowserDriver("/home/biwxx/Downloads/tor-browser_en-US") as driver:
+		driver.load_url('https://invidio.us/watch?v=fURWLTvZzhY', wait_for_page_body=True)
+    		time.sleep(4)
+    #play = driver.find_element_by_class_name('autoplay skeleton-light-border-bottom')
+    #play.click();
+    		play_button = driver.find_element_by_class_name("vjs-poster")
+    		play_button.click()
+    		time.sleep(20)
+    		driver.close();
+    
+    
